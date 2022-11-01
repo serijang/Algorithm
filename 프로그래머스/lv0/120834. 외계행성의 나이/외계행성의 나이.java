e2be4db14c2a969c12b9d2a8f033d69ecdcb1166
SERI <103922738+serijang@users.noreply.github.com>
2022-11-01 14:08:58 +0900

    Update 외계행성의 나이.java

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -8,3 +8,20 @@ class Solution {
         return String.valueOf(answer);
     }
 }
+
+// 다른 풀이
+class Solution {
+    public String solution(int age) {
+        String ageToOutsideAge = Integer.toString(age);
+        String result = "";
+
+
+        for(int i = 0; i < ageToOutsideAge.length(); i++)
+        {
+            result += (char)(ageToOutsideAge.charAt(i) + 49);
+
+        }
+
+        return result;
+    }
+}
