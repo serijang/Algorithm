f8a82c1d28a5040592c13c296c5d2d5bdb919705
SERI <103922738+serijang@users.noreply.github.com>
2022-11-01 15:07:47 +0900

    Update 암호 해독.java

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -9,3 +9,16 @@ class Solution {
         return answer;
     }
 }
+
+
+// 다른 풀이
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for (int i = code; i <= cipher.length(); i = i + code) {  // 처음부터 i를 code의 수로 잡고, i += code로 증가.
+            answer += cipher.substring(i - 1, i);
+        }
+        return answer;
+    }
+}
