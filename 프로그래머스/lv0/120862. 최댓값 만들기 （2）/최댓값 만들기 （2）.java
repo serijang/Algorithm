c3d2be3b162b61a82162cee141b05e41d7974104
SERI <103922738+serijang@users.noreply.github.com>
2022-11-01 12:23:18 +0900

    Update 최댓값 만들기 （2）.java

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -13,3 +13,15 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);  // 정렬
+        return numbers[numbers.length-1] * numbers[numbers.length-2] < numbers[0] * numbers[1] ? 
+            numbers[0] * numbers[1] : numbers[numbers.length-1] * numbers[numbers.length-2];
+    }
+}
+// 음수중에 절대값이 가장 큰 수 두개의 곱과 양수 중 가장 큰 2개의 수 곱을 비교
