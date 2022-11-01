a6d5f2511f97fcc8f857cd6a9854c37742b64a6d
SERI <103922738+serijang@users.noreply.github.com>
2022-11-01 16:22:42 +0900

    Update 369게임.java

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         String temp = String.valueOf(order);
         for(int i = 0; i<= temp.length()-1; i++) {
-            if(temp.charAt(i) != '0' && temp.charAt(i) % 3 == 0) {
+            if(temp.charAt(i) != '0' && temp.charAt(i) % 3 == 0) { // 0을 '' 문자열 처리 안해줘서 시간낭비했음.
                 answer++;
             }
         }
