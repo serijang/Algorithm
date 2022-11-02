129b49df3052620c25ae73395dbddeb80aaf3afd
SERI <103922738+serijang@users.noreply.github.com>
2022-11-02 14:23:00 +0900

    Update 2차원으로 만들기.java

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -12,3 +12,20 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = {};
+
+        int length = num_list.length;
+
+        answer = new int[length/n][n];
+
+        for(int i=0; i<length; i++){  // for문 한번만 사용
+            answer[i/n][i%n]=num_list[i];   // 나누기와 나머지.... 어렵다!
+        }
+
+        return answer;
+    }
+}
